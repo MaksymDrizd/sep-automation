396c1d30f5083edcab104ef5ab56a9a60c2a4c06
MaksymDrizd <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>
2024-10-03 14:52:48 -0400

    sep11 feature

--- steps/getting-started/startAndRefundDates.js
+++ steps/getting-started/startAndRefundDates.js
@@ -5,16 +5,31 @@ import { productInfo } from "../../utilities/qa-data-reader.js";
 
 
 Given('user is on the no enrollment page', async function () {
+    await startApplicationPage.login();
 });
 
 Then('the program start date is displayed', async function () {
+    await expect(startApplicationPage.programStartDate).toBeVisible();
 });
 
 Then('the program refund date is displayed', async function () {
+    await expect(startApplicationPage.refundEndDate).toBeVisible();
 });
 
 Then('the displayed start date for the program is correct', async function () {
+    const ACTUAL_START_DATE = await startApplicationPage.programStartDate.innerText();
+    const EXPECTED_START_DATE = productInfo.startDate;
+        //console.log(`Actual start date: ${ACTUAL_START_DATE}`);
+        //console.log(`Expected start date: ${EXPECTED_START_DATE}`);
+        //console.log(`Expected Upfront price: ${productInfo.prices[0].baseAmount}`);
+        //console.log(`Expected Upfront discout amount: ${productInfo.prices[0].upfrontDiscountAmount}`);
+    expect(ACTUAL_START_DATE).toBe(EXPECTED_START_DATE);
 });
 
 Then('the displayed refund date for the program is correct', async function () {
+    const ACTUAL_REFUND_DATE = await startApplicationPage.refundEndDate.innerText();
+    const EXPECTED_REFUND_DATE = productInfo.refundDate;
+        //console.log(`Actual refund date: ${ACTUAL_REFUND_DATE}`);
+        //console.log(`Expected refund date: ${EXPECTED_REFUND_DATE}`);
+    expect(ACTUAL_REFUND_DATE).toBe(EXPECTED_REFUND_DATE);
 });
